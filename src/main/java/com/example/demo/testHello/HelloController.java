17cfa957e90e9feec2c1f145e4a88b8c655d48c5
120640519@qq.com <bietianshenh2o>
2019-05-05 16:08:18 +0800

    init

--- src/main/java/com/example/demo/testHello/HelloController.java
+++ src/main/java/com/example/demo/testHello/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String test1(String name){
-        return "hello "+ name;
+        return "hello222 "+ name;
     }
 }
